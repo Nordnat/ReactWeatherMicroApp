6d1a903d3ba87221e5db09a7517739bb989b58f7
Nordnat <rkamieniarz@gmail.com>
2018-01-30 03:57:25 +0100

    foundation styling

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,12 +4,24 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = () => {
     return (
-        <div>
-            <h2>Nav Component</h2>
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-            <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-            <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+        <div className="grid-container full">
+            <div className="top-bar">
+                <div className="top-bar-left">
+                <ul className="menu medium-horizontal">
+                    <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+                    <li><Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+                    <li><Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+                </ul>
+                </div>
+                <div className="top-bar-right">
+                    <ul className="menu">
+                        <li><input type="search" placeholder="Search"></input></li>
+                        <li><button type="button" className="button">Get the weather</button></li>
+                    </ul>
+                </div>
+            </div>
         </div>
+        
     );
 }
 
